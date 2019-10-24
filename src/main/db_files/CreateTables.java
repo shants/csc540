1ed1a2c46bddd8d7659a03fe63541f2e6e570968
rvsingh31 <ravinder.singh3196@gmail.com>
2019-10-24 02:11:37 -0400

    Updated tables.sql

--- src/main/db_files/CreateTables.java
+++ src/main/db_files/CreateTables.java
@@ -28,31 +28,30 @@ public class CreateTables
 //            System.out.println("Database 'hospital' created successfully");
 //        }
 //    }
-//
-//    public void executeCreateQuery(String query, String table_name){
-//        Connection connection = new DatabaseConnection().getConnection();
-//        Statement statement = null;
-//        try {
-//            statement = connection.createStatement();
-//            statement.executeUpdate(query);
-//            if (statement != null){
-//                try {
-//                    statement.close();
-//                } catch (SQLException e) {
-//                    System.out.println("Unable to close the sql statement:"+ e.getMessage());
-//                }
-//            }
-//            System.out.println("Table " + table_name+ " created successfully");
-//        } catch (SQLException e) {
-//            System.out.println("Unable to create a new table:"+ e.getMessage());
-//        }
-//    }
-//
-//    public void createFacilityClassification(){
-//        String query = "CREATE TABLE IF NOT EXISTS facility_classification("
-//                +" classification_id NUMBER(10) NOT NULL,"
-//                +" classification_type VARCHAR2(20) NOT NULL, "
-//                +")";
-//        this.executeCreateQuery(query,"facility_classification");
-//    }
+
+    public void executeCreateQuery(String query, String table_name){
+        Connection connection = new DatabaseConnection().getConnection();
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            try {
+                    statement.close();
+            }
+            catch (SQLException e) {
+                System.out.println("Unable to close the sql statement:" + e.getMessage());
+            }
+            System.out.println("Table " + table_name+ " created successfully");
+        } catch (SQLException e) {
+            System.out.println("Unable to create a new table:"+ e.getMessage());
+        }
+    }
+
+    public void createFacilityClassification(){
+        String query = "CREATE TABLE demo_test_table ( "+
+                "id int NOT NULL, "+
+                "name varchar(255) NOT NULL, "+
+                "  PRIMARY KEY (id))";
+        this.executeCreateQuery(query,"demo_test_table");
+    }
 }
--- src/resources/tables.sql
+++ src/resources/tables.sql
@@ -8,14 +8,14 @@ CONSTRAINT classification_key PRIMARY KEY(classification_id));
 CREATE SEQUENCE fc_cls_seq START WITH 1;
 
 CREATE OR REPLACE TRIGGER classification_trigger
-BEFORE INSERT ON facility_classification
+BEFORE INSERT ON FACILITY_CLASSIFICATION
 FOR EACH ROW
-
 BEGIN
   SELECT fc_cls_seq.NEXTVAL
-  INTO   :new.id
+  INTO   :new.classification_id
   FROM   dual;
 END;
+/
 
 CREATE TABLE street(
 street_id NUMBER(10) NOT NULL,
@@ -32,9 +32,10 @@ FOR EACH ROW
 
 BEGIN
   SELECT street_seq.NEXTVAL
-  INTO   :new.id
+  INTO   :new.street_id
   FROM   dual;
 END;
+/
 
 CREATE TABLE city(
 city_id NUMBER(10) NOT NULL,
@@ -51,9 +52,10 @@ FOR EACH ROW
 
 BEGIN
   SELECT city_seq.NEXTVAL
-  INTO   :new.id
+  INTO   :new.city_id
   FROM   dual;
 END;
+/
 
 CREATE TABLE state(
 state_id NUMBER(10) NOT NULL,
@@ -70,9 +72,10 @@ FOR EACH ROW
 
 BEGIN
   SELECT state_seq.NEXTVAL
-  INTO   :new.id
+  INTO   :new.state_id
   FROM   dual;
 END;
+/
 
 CREATE TABLE country(
 country_id NUMBER(10) NOT NULL,
@@ -89,7 +92,83 @@ FOR EACH ROW
 
 BEGIN
   SELECT country_seq.NEXTVAL
-  INTO   :new.id
+  INTO   :new.country_id
   FROM   dual;
 END;
+/
 
+CREATE TABLE certification(
+certification_id NUMBER(10) NOT NULL,
+certification_name VARCHAR2(50) NOT NULL);
+
+ALTER TABLE certification ADD (
+CONSTRAINT certification_key PRIMARY KEY(certification_id));
+
+CREATE SEQUENCE certification_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER certification_trigger
+BEFORE INSERT ON certification
+FOR EACH ROW
+
+BEGIN
+  SELECT certification_seq.NEXTVAL
+  INTO   :new.certification_id
+  FROM   dual;
+END;
+/
+
+CREATE TABLE facility_address(
+facility_address_id NUMBER(10) NOT NULL,
+street_num VARCHAR2(50) NOT NULL,
+street_id NUMBER(10) NOT NULL,
+city_id NUMBER(10) NOT NULL,
+state_id NUMBER(10) NOT NULL,
+country_id NUMBER(10) NOT NULL,
+facility_id NUMBER(10) NOT NULL,
+CONSTRAINT facility_address_key PRIMARY KEY (facility_address_id),
+CONSTRAINT fk_fa_street FOREIGN KEY (street_id) REFERENCES street(street_id),
+CONSTRAINT fk_fa_city FOREIGN KEY (city_id) REFERENCES city(city_id),
+CONSTRAINT fk_fa_state FOREIGN KEY (state_id) REFERENCES state(state_id),
+CONSTRAINT fk_fa_country FOREIGN KEY (country_id) REFERENCES country(country_id)
+);
+
+CREATE SEQUENCE facility_address_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER facility_address_trigger
+BEFORE INSERT ON facility_address
+FOR EACH ROW
+
+BEGIN
+  SELECT facility_address_seq.NEXTVAL
+  INTO   :new.facility_address_id
+  FROM   dual;
+END;
+/
+
+CREATE TABLE medical_facility(
+facility_id NUMBER(10) NOT NULL,
+facility_name VARCHAR2(50) NOT NULL,
+capacity NUMBER(20) NOT NULL,
+classification_id NUMBER(10) NOT NULL,
+facility_address_id NUMBER (10) NOT NULL,
+CONSTRAINT facility_key PRIMARY KEY (facility_id),
+CONSTRAINT fk_mf_cs FOREIGN KEY (classification_id) REFERENCES facility_classification(classification_id),
+CONSTRAINT fk_mf_fa FOREIGN KEY (facility_address_id) REFERENCES facility_address(facility_address_id)
+);
+
+CREATE SEQUENCE medical_facility_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER medical_facility_trigger
+BEFORE INSERT ON medical_facility
+FOR EACH ROW
+
+BEGIN
+  SELECT medical_facility_seq.NEXTVAL
+  INTO   :new.facility_id
+  FROM   dual;
+END;
+/
+
+ALTER TABLE facility_address ADD (
+CONSTRAINT fk_fa_mf FOREIGN KEY (facility_id) REFERENCES medical_facility(facility_id)
+);
