daf83004358cfbadf68ebb521051e5720942a37f
shants <mailtoshants@gmail.com>
2019-10-24 18:17:46 -0400

    Message utils for patient_routing

--- src/main/java/Utils/MessageUtils.java
+++ src/main/java/Utils/MessageUtils.java
@@ -1,12 +1,14 @@
-package main.java.workflow;
-public class MessagesUtil {
+package main.java.Utils;
+
+public class MessageUtils {
     public static String GLOBAL_ENTER_OPTION = "Enter Option";
     public static String PATIENT_ROUTING_CHECK_IN="Check In";
     public static String PATIENT_ROUTING_CHECK_OUT_ACK="Check Out Acknowledgement";
     public static String PATIENT_ROUTING_GO_BACK="Go Back";
 
-    enum PATIENT_ROUTING {
+    public enum PATIENT_ROUTING {
         PATIENT_ROUTING_CHECKIN, PATIENT_ROUTING_CHECK_OUT, PATIENT_ROUTING_GO_BACK;
     }
 
 }
+
