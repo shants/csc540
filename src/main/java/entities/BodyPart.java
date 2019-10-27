d1a8c3609fd126a859de7e3701fc7de98ed5a56f
shants <mailtoshants@gmail.com>
2019-10-27 01:10:24 -0400

    add body_part and Staff entity

---
+++ src/main/java/entities/BodyPart.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class BodyPart {
+    private String body_part;
+    private String name;
+
+    public String getBody_part() {
+        return body_part;
+    }
+
+    public void setBody_part(String body_part) {
+        this.body_part = body_part;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/entities/Staff.java
@@ -0,0 +1,51 @@
+package entities;
+
+import java.util.Date;
+
+public class Staff {
+    private int staff_id;
+    private String name;
+    private Date hire_date;
+    private String designation;
+    private int type_id;
+
+    public int getStaff_id() {
+        return staff_id;
+    }
+
+    public void setStaff_id(int staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getHire_date() {
+        return hire_date;
+    }
+
+    public void setHire_date(Date hire_date) {
+        this.hire_date = hire_date;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(int type_id) {
+        this.type_id = type_id;
+    }
+}
