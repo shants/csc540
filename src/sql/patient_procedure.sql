fa4d8fdfcc355e4caa50a5cec561607abfa2debb
shants <mailtoshants@gmail.com>
2019-11-14 18:17:26 -0500

    new_branch: patient_proc

--- src/sql/patient_procedure.sql
+++ src/sql/patient_procedure.sql
@@ -5,49 +5,49 @@ CREATE OR REPLACE PROCEDURE create_new_patient_tables
 AUTHID CURRENT_USER IS
 new_query varchar2(5000);
 BEGIN
-new_query := 'CREATE TABLE patient_address_'|| to_char(facility_id) || '(
+new_query := 'CREATE TABLE patient_address(
 patient_address_id NUMBER(10) NOT NULL,
 street_num VARCHAR2(50) NOT NULL,
 street_id NUMBER(10) NOT NULL,
 city_id NUMBER(10) NOT NULL,
 state_id NUMBER(10) NOT NULL,
 country_id NUMBER(10) NOT NULL,
-CONSTRAINT patient_address_'|| to_char(facility_id) || '_key PRIMARY KEY (patient_address_id),
-CONSTRAINT fk_pa_street_'|| to_char(facility_id) || ' FOREIGN KEY (street_id) REFERENCES street(street_id),
-CONSTRAINT fk_pa_city_'|| to_char(facility_id) || ' FOREIGN KEY (city_id) REFERENCES city(city_id),
-CONSTRAINT fk_pa_state_'|| to_char(facility_id) || ' FOREIGN KEY (state_id) REFERENCES state(state_id),
-CONSTRAINT fk_pa_country_'|| to_char(facility_id) || ' FOREIGN KEY (country_id) REFERENCES country(country_id)
+CONSTRAINT patient_address_key PRIMARY KEY (patient_address_id),
+CONSTRAINT fk_pa_street FOREIGN KEY (street_id) REFERENCES street(street_id),
+CONSTRAINT fk_pa_city FOREIGN KEY (city_id) REFERENCES city(city_id),
+CONSTRAINT fk_pa_state FOREIGN KEY (state_id) REFERENCES state(state_id),
+CONSTRAINT fk_pa_country FOREIGN KEY (country_id) REFERENCES country(country_id)
 )';
 EXECUTE IMMEDIATE new_query;
-new_query := 'CREATE SEQUENCE patient_address_'|| to_char(facility_id) || '_seq START WITH 1';
+new_query := 'CREATE SEQUENCE patient_address_seq START WITH 1';
 EXECUTE IMMEDIATE new_query;
-new_query := 'CREATE OR REPLACE TRIGGER patient_address_'|| to_char(facility_id) || '_trigger
-BEFORE INSERT ON patient_address_'|| to_char(facility_id) || '
+new_query := 'CREATE OR REPLACE TRIGGER patient_address_trigger
+BEFORE INSERT ON patient_address
 FOR EACH ROW
 BEGIN
-  SELECT patient_address_'|| to_char(facility_id) || '_seq.NEXTVAL
+  SELECT patient_address_seq.NEXTVAL
   INTO   :new.patient_address_id
   FROM   dual;
 END;';
 EXECUTE IMMEDIATE new_query;
-new_query := 'CREATE TABLE patient_'|| to_char(facility_id) ||
-                    ' (patient_id NUMBER(10) NOT NULL, ' ||
-                    ' first_name VARCHAR2(30) NOT NULL, ' ||
-                    ' last_name VARCHAR2(30) NOT NULL, ' ||
-                    ' date_of_birth DATE NOT NULL, ' ||
-                    ' phone_number NUMBER(10) NOT NULL, ' ||
-                    ' patient_address_id NUMBER(30) NOT NULL, ' ||
-                    'CONSTRAINT patient_'|| to_char(facility_id) || '_key PRIMARY KEY (patient_id),' ||
-                    ' CONSTRAINT fk_pt_pa_'|| to_char(facility_id) || ' FOREIGN KEY (patient_address_id) REFERENCES patient_address_'
-                    || to_char(facility_id) || ' (patient_address_id))';
+new_query := 'CREATE TABLE patient
+                    (patient_id NUMBER(10) NOT NULL,
+                     facility_id NUMBER(10) NOT NULL,
+                    first_name VARCHAR2(30) NOT NULL,
+                    last_name VARCHAR2(30) NOT NULL,
+                    date_of_birth DATE NOT NULL,
+                    phone_number NUMBER(10) NOT NULL,
+                    patient_address_id NUMBER(30) NOT NULL,
+                    CONSTRAINT patient_key PRIMARY KEY (patient_id),
+                    CONSTRAINT fk_pt_pa FOREIGN KEY (patient_address_id) REFERENCES patient_address (patient_address_id))';
 EXECUTE IMMEDIATE new_query;
-new_query := 'CREATE SEQUENCE patient_'|| to_char(facility_id) || '_seq START WITH 1';
+new_query := 'CREATE SEQUENCE patient_seq START WITH 1';
 EXECUTE IMMEDIATE new_query;
-new_query := 'CREATE OR REPLACE TRIGGER patient_'|| to_char(facility_id) || '_trigger
-BEFORE INSERT ON patient_'|| to_char(facility_id) || '
+new_query := 'CREATE OR REPLACE TRIGGER patient_trigger
+BEFORE INSERT ON patient
 FOR EACH ROW
 BEGIN
-  SELECT patient_'|| to_char(facility_id) || '_seq.NEXTVAL
+  SELECT patient_seq.NEXTVAL
   INTO   :new.patient_id
   FROM   dual;
 END;';
