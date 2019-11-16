24b3a8750ffcacc269fbe4e43701bfb041a081cd
Tapas Mallick <tapas1306@gmail.com>
2019-11-15 21:19:48 -0500

    default value for negative experience

--- src/main/java/workflow/StaffPatientReport.java
+++ src/main/java/workflow/StaffPatientReport.java
@@ -32,6 +32,9 @@ public class StaffPatientReport extends IScreen {
         rep.setVisit_id(ViewerContext.getInstance().getPatientToCheckout().getVisit_id());
         rep.setService_code(1);
         rep.setStaff_id(ViewerContext.getInstance().getValue(ViewerContext.IDENTIFIER_TYPES.STAFF_ID));
+        // Default negative experience
+        rep.setNegative_experience_value(0);
+        rep.setNegative_experience_text("No Negative Experience");
         ViewerContext.getInstance().setPatientReport(rep);
     }
 
